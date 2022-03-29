e358f31753d39a818d1eaee48858c543798856ab
Adib Zefar <adibzefar@gmail.com>
2022-03-29 18:12:54 -0400

    Better formatting

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed:", actual," === ",expected);
-  } else {
-    console.log("Assertion Failed:", actual,"!=== ",expected);
-  }
+    if (actual === expected){
+        console.log("Assertion Passed:", actual," === ",expected)
+    }else{
+        console.log("Assertion Failed:", actual,"!=== ",expected)
+    }
 };
 
 // TEST CODE
