b601b58a6d75909d3b2c6e26448a21d2d436ffd4
Adib Zefar <adibzefar@gmail.com>
2022-04-02 18:55:05 -0400

    files updated

---
+++ tail.js
@@ -0,0 +1,13 @@
+// // FUNCTION IMPLEMENTATION
+// const assertEqual = function(array) {
+//     console.log(array.slice((array.length*-1)+1));
+// };
+
+// // TEST CODE
+// assertEqual([]);
+// assertEqual(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(["1", "2", "2"]);
+
+console.log(friend);
+var friend = 'E.T.';
+console.log(friend);
