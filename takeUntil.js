fa99903d0b21a084146866dd498291ea5e8ff976
Adib Zefar <adibzefar@gmail.com>
2022-04-11 22:33:20 -0400

    added the assert equal function

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,21 @@
+// eqArrays FUNCTION IMPLEMENTATION
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length ; i++) {
+    if (actual[i] !== expected[i]) {
+      return false
+    }
+  } return true
+}
+
+// assertEqual FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+  console.log(`Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`Assertion Failed: ${actual} !=== ${expected}`)
+  }
+};
+
 const takeUntil = function(array, callback) {
   // ...
   const results = [];
@@ -22,3 +40,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertEqual(data1,results1);
+assertEqual(data2,results2);
