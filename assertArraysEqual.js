72b3163bf9ab9c2fdc4bc707a0f15fe266093537
Adib Zefar <adibzefar@gmail.com>
2022-04-18 03:45:05 -0400

    assertArraysEqual.js updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,8 +19,5 @@ const assertArraysEqual  = function(actual, expected) {
 };
 
 // TEST CODE
-// console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-// console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
