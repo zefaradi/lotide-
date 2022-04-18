c7d0f47d8ad177b1fbf75fab7a95ef75b29d49bc
Adib Zefar <adibzefar@gmail.com>
2022-04-18 03:37:19 -0400

    eqArrays.js updated

--- eqArrays.js
+++ eqArrays.js
@@ -3,18 +3,19 @@ const eqArrays = function(actual, expected) {
   for (let i = 0; i < actual.length ; i++) {
     if (actual[i] !== expected[i]) {
       return false
-      break
     }
   } return true
 }
 
 // assertEqual FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
-  console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }else{
-    console.log(`Assertion Failed: ${actual} !=== ${expected}`)
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
+    
 };
 
 // TEST CODE
