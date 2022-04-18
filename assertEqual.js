dcd79dfc72818850a61abb28cb0cef54bcd575c1
Adib Zefar <adibzefar@gmail.com>
2022-04-18 03:24:16 -0400

    assertEqual.js updated

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,13 @@
 // FUNCTION IMPLEMENTATION
+
 const assertEqual = function(actual, expected) {
-    if (actual === expected){
-        console.log(`Assertion Passed: ${actual} === ${expected}`)
-    }else{
-        console.log(`Assertion Failed: ${actual} !=== ${expected}`)
-    }
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+    
 };
 
 // TEST CODE
