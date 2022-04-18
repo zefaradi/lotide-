15180208eaf0026ba197cbc8ace46ebdf4856e23
Adib Zefar <adibzefar@gmail.com>
2022-04-18 03:50:45 -0400

    without.js updated

--- without.js
+++ without.js
@@ -1,34 +1,38 @@
-// without FUNCTION IMPLEMENTATION
-const without = function(arr, rm) {
-  let arr2 = arr.slice()
-  for (let i = 0; i < arr.length ; i++) {
-    for (let j = 0; j < rm.length ; j++){
-      if (arr[i] === rm[j]) {
-        arr2.splice(i, 1);
-    }
-  }
-  } return arr2
-}
-
 // eqArrays FUNCTION IMPLEMENTATION
 const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length ; i++) {
+  for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
-      return `${actual} not equal to ${expected}`
+      return false;
     }
-  } return `${actual} equal to ${expected}`
-}
+  } return true;
+};
 
-// assertEqual FUNCTION IMPLEMENTATION
-// const assertArraysEqual = function(actual, expected) {
-//   if (actual === expected){
-//   console.log(`Assertion Passed: ${actual} === ${expected}`)
-//   }else{
-//     console.log(`Assertion Failed: ${actual} !=== ${expected}`)
-//   }
-// };
+// assertArraysEqual  FUNCTION IMPLEMENTATION
+const assertArraysEqual  = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+    
+};
+
+// without FUNCTION IMPLEMENTATION
+const without = function(arr, rm) {
+  let arr2 = arr.slice();
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < rm.length; j++) {
+      if (arr[i] === rm[j]) {
+        arr2.splice(i, 1);
+      }
+    }
+  } return arr2;
+};
+
 
 // TEST CODE
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-console.log(eqArrays(words, ["hello", "world", "lighthouse"]));
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
