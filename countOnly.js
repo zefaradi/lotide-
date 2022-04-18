5787f2ba033ba640fb6349395cd0db041ac91f46
Adib Zefar <adibzefar@gmail.com>
2022-04-18 04:28:12 -0400

    countOnly.js added

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,19 @@
-// FUNCTION IMPLEMENTATION
+// Assert Equal IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
-      console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }else{
-      console.log(`Assertion Failed: ${actual} !=== ${expected}`)
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
+    
 };
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-    if (itemsToCount[item]){
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -20,7 +22,7 @@ const countOnly = function(allItems, itemsToCount) {
     }
   }
   return results;
-}
+};
 // TEST CODE
 const firstNames = [
   "Karl",
