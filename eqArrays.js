59c56503dfbbfd5acbfc867e6d0a878b9b57819f
Adib Zefar <adibzefar@gmail.com>
2022-04-18 03:38:02 -0400

    eqArrays.js updated

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,11 @@
 // eqArrays FUNCTION IMPLEMENTATION
 const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length ; i++) {
+  for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
-      return false
+      return false;
     }
-  } return true
-}
+  } return true;
+};
 
 // assertEqual FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
@@ -23,4 +23,4 @@ const assertEqual = function(actual, expected) {
 // console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
