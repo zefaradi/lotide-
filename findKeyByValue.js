a58d27bd684de8d767d276d6f00fd023b107ede5
Adib Zefar <adibzefar@gmail.com>
2022-04-18 18:24:14 -0400

    findKeyByValue.js added

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,12 @@
-// FUNCTION IMPLEMENTATION
+// ASSERT EQUAL FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
-      console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }else{
-      console.log(`Assertion Failed: ${actual} !=== ${expected}`)
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
+    
 };
 
 const findKeyByValue = (object, value) => {
