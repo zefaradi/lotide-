44b881d83d4340942abd94d9a1f4c9f59548104b
Adib Zefar <adibzefar@gmail.com>
2022-04-18 18:13:06 -0400

    countLetters.js added

--- countLetters.js
+++ countLetters.js
@@ -1,15 +1,19 @@
-// FUNCTION IMPLEMENTATION
+// Assert Equal IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
-      console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }else{
-      console.log(`Assertion Failed: ${actual} !=== ${expected}`)
+  for (let key in actual) {
+    if (actual[key] === expected[key]) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+      break;
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+      break;
+    }
   }
 };
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countLetters = function(words) {
-  let newWord = words.replace(/\s/g, '')
+  let newWord = words.replace(/\s/g, '');
   // console.log("newWord: ", newWord)
   const results = {};
   for (const item of newWord) {
@@ -20,10 +24,10 @@ const countLetters = function(words) {
     }
   }
   return results;
-}
+};
 
 //TEST
 
-console.log(countLetters("LHL"));
-console.log(countLetters("lighthouse in the house"));
+assertEqual(countLetters("LHL"),{ L: 2, H: 1 });
+assertEqual(countLetters("lighthouse in the house"),{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
 
