56fda562550874a396e366758b11e9aa61e1a2dc
Adib Zefar <adibzefar@gmail.com>
2022-04-18 04:23:29 -0400

    middle.js updated

--- middle.js
+++ middle.js
@@ -1,12 +1,22 @@
-// assertEqual FUNCTION IMPLEMENTATION
-
+// eqArrays FUNCTION IMPLEMENTATION
 const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length ; i++) {
+  for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
-      return `${actual} not equal to ${expected}`
+      return false;
     }
-  } return `${actual} equal to ${expected}`
-}
+  } return true;
+};
+
+// assertArraysEqual  FUNCTION IMPLEMENTATION
+const assertArraysEqual  = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+    
+};
 
 
 // Middle FUNCTION IMPLEMENTATION
@@ -14,20 +24,18 @@ const middle = function(arr) {
   let newArr = [];
   let index;
   if (arr.length <= 2) {
-    return newArr
+    return newArr;
   } else if (arr.length % 2 !== 0) {
-    index = Math.round(arr.length/2) - 1;
-    newArr.push(arr[index])
-    return newArr
-  }else {
-    index = Math.round(arr.length/2)
-    newArr.push(arr[index -1 ], arr[index]);
-    // newArr.push(arr[index]);
-    return newArr
+    index = Math.round(arr.length / 2) - 1;
+    newArr.push(arr[index]);
+    return newArr;
+  } else {
+    index = Math.round(arr.length / 2);
+    newArr.push(arr[index - 1 ], arr[index]);
+    return newArr;
   }
-}
+};
 
 
 // TEST CODE
-console.log(middle([1,2,3,4,5,6]));
-console.log((eqArrays([1,2,3,4,5,6], [1,2,3,4,5,6])));
+assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
