3dd38db157a2825a366910048d5980002b139f3a
Adib Zefar <adibzefar@gmail.com>
2022-04-19 14:16:00 -0400

    middleTest.js updated

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,23 @@
 // Middle, assertArraysEqual and eqArrays FUNCTION IMPLEMENTATION
 
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const eqArrays = require('../eqArrays');
+// const middle = require('../middle');
+
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns [3,4] for [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+  });
 
-// TEST CODE
-assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
+  it("returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+});
+
+
+// // TEST CODE
+// assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
