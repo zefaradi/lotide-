204b94cce4b3adfdbf0d9c3a647c8e64549fb1c3
Adib Zefar <adibzefar@gmail.com>
2022-04-19 13:29:36 -0400

    assertArraysEqualTest.js added

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+// assertArraysEqual and eqArrays FUNCTION IMPLEMENTATION
+
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+
+// TEST CODE
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
