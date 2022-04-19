1498449b4df88a15bdc684a91a5081a4d764476f
Adib Zefar <adibzefar@gmail.com>
2022-04-19 13:36:17 -0400

    middleTest.js added

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+// Middle, assertArraysEqual and eqArrays FUNCTION IMPLEMENTATION
+
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+const middle = require('../middle');
+
+
+// TEST CODE
+assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
